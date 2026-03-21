fb3a94a02bc1e13dee5f10a3fe8bfd3cc59f3664
music_project <ankitgupta37937@gmail.com>
2026-03-21 16:57:02 +0530

    changes in routes/auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const Otp = require('../models/OTP');
+const Otp = require('../models/otp');
 
 
 // ============================
