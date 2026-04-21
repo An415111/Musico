3d7c477d31697aa35a2db432f41192caf90870b8
music_project <ankitgupta37937@gmail.com>
2026-04-21 11:15:17 +0530

    fixed cookie issue for production

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -160,8 +160,8 @@ router.post('/login', async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000  // ← ADD THIS (7 days in milliseconds)
     });
     res.redirect("/music");
